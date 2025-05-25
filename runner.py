9743ae72c7ec69c28d0179b6bea16ddb29a911d4
Khanh Nguyen <khanhnd185@gmail.com>
2025-05-25 22:25:36 +0900

    update

--- runner.py
+++ runner.py
@@ -1,14 +1,111 @@
 from warewolf import Warewolf
+from warewolf import Role, Result, Stat
 
 class Runner:
   def __init__(self):
     self.game = Warewolf()
+    self.players = []
     
+  def get_num_player(self):
+    num_player = int(input("Number of players: "))
+    return num_player
+
+  def get_player_name(self, m):
+    name = input(m)
+    return name
+
+  def get_yes_no(self, m):
+    yes = input(m)
+    return yes in ["yes","Yes","y","Y","OK","Ok","ok"]
+
+  def message(self, m):
+    print(m)
+
   def run(self):
-    
-    is_done = False
-    
-    while not is_done:
-      # Do something
+    self.message("Let us start the game!")
+    num_player = self.get_num_player()
+
+    self.message("Getting Started...")
+    for n in range(num_player):
+      name = self.get_player_name(f"Name of player {n+1}: ")
+      self.game.add_player(name)
+      self.players.append(name)
+
+    self.message("Everyone sleep..")
+    for role in [
+      Role.DOCTOR
+      , Role.WOLF
+      , Role.WITCH
+      , Role.SEER
+    ]:
+      self.message(f"{role} wake up.")
+      name = self.get_player_name(f"Who is {role}: ")
       
-      is_done = self.game.is_done()
+      self.game.set_player_role(name, role)
+      self.message(f"{role} sleep.")
+
+    result  = Result.NOTDONE
+    night   = 0
+    poison  = None
+    saved   = None
+    while result == Result.NOTDONE:
+      cured   = None
+      killed  = None
+      night  += 1
+      self.message(f"\nNight {night}")
+
+      while True:
+        if night==1:
+          doctor = self.get_player_name("Doctor wake up. ")
+          self.game.set_player_role(doctor, Role.DOCTOR)
+        else:
+          self.message("Doctor wake up. ")
+        name = self.get_player_name("Who you will cure: ")
+        if name in self.players and name != cured:
+          break
+      if self.game.is_dead(doctor): cured = name
+      self.message("Doctor sleep")
+
+      while True:
+        name = self.get_player_name("Wolves wake up. Who you will kill: ")
+        if name in self.players and self.game.is_alive(name): break
+      killed = name
+      self.message("Wolves sleep")
+
+      if self.get_yes_no(f"Witch wake up. {killed} is killed, you wanna save? "):
+        if saved == None:
+          saved  = killed
+          killed = None
+      name = self.get_player_name("Who you will poison: ")
+      if name in self.players and self.game.is_alive(name):
+        if poison == None:
+          poison = name
+      self.message("Witch sleep")
+
+      while True:
+        name = self.get_player_name("Seer wake up. Choose a person to scan: ")
+        if name in self.players and self.game.is_alive(name):
+          role = Role.WOLF if self.game.is_wolf(name) else Role.VILLAGER
+          self.message(f"{name} is a {role}")
+          break
+
+      victim = []
+      if killed and killed!=cured:
+        self.game.kill_player(killed)
+        victim.append(killed)
+      if poison and poison!=killed:
+        self.game.kill_player(poison)
+        victim.append(poison)
+      self.message(f"Last night {len(victim)} dead. They are {victim}")
+
+      name = self.get_player_name("Let us vote a person to hang: ")
+      if name in self.players:
+        self.game.kill_player(name)
+      result = self.game.is_done()
+
+    self.message(f"{result}")
+
+
+if __name__ == '__main__':
+  runner = Runner()
+  runner.run()
--- warewolf.py
+++ warewolf.py
@@ -1,18 +1,23 @@
 from enum import Enum
 
 class Stage(Enum):
-  NIGHT = 0
-  DAY   = 1
+  NIGHT_BEGIN  = 0
+  DOCTOR_AWAKE = 1
+  WOLF_AWAKE   = 2
+  WITCH_AWAKE  = 3
+  SEER_AWAKE   = 4
+  VOTING       = 5
+  NUM_STAGES   = 6
 
 class Stat(Enum):
-  ACTIVE = 0
-  DEAD   = 1
+  ALIVE = 0
+  DEAD  = 1
 
 class Result(Enum):
-  NOTDONE     = 0
-  WOLFWIN     = 1
-  VILLAGERWIN = 2
-  TIE         = 3
+  NOTDONE     = "Game is going"
+  WOLFWIN     = "Wolf win"
+  VILLAGERWIN = "Villager win"
+  TIE         = "Tie"
 
 class Role(Enum):
   WOLF         = "wolf"     # soi
@@ -23,43 +28,81 @@ class Role(Enum):
 
 
 class Player:
-  def __init__(self, name, stat=Stat.ACTIVE):
-    self.role = None
+  def __init__(self, name
+    , stat=Stat.ALIVE
+    , role=Role.VILLAGER
+  ):
+    self.role = role
     self.name = name
     self.stat = stat
         
   def set_name(self, name): self.name = name
   def set_stat(self, stat): self.stat = stat
   def set_role(self, role): self.role = role
+  def is_alive(self): return self.stat==Stat.ALIVE
+  def is_dead(self) : return self.stat==Stat.DEAD
 
 
 class Warewolf:
   def __init__(self):
     self.players = {}
-    self.stage  = Stage.NIGHT
+    self.names  = []
+    self.stage  = Stage.NIGHT_BEGIN
     self.rounds = 0
-    self.player = None
+    self.witch  = None
+    self.doctor = None
+    self.seer   = None
+    self.wolves = []
 
   def add_player(self, name):
     self.players[name] = Player(name)
+    self.names.append(name)
 
   def set_player_role(self, name, role):
-    for player in self.players:
-      if player.name == name:
-        player.set_role(role)
-        break
+    if name in self.players:
+      self.players[name].set_role(role)
+
+      if   role==Role.WITCH : self.witch  = name
+      elif role==Role.DOCTOR: self.doctor = name
+      elif role==Role.SEER  : self.seer   = name
+      elif role==Role.WOLF  : self.wolves.append(name) 
+
+  def kill_player(self, name):
+    if name in self.players:
+      self.players[name].set_stat(Stat.DEAD)
+
+  def is_alive(self, name):
+    if name in self.players:
+      return self.players[name].is_alive()
+    return False
+
+  def is_dead(self, name):
+    if name in self.players:
+      return self.players[name].is_dead()
+    return True
+
+  def is_valid(self, name):
+    if name in self.players:
+      return True
+    return False
+
+  def is_wolf(self, name):
+    return self.players[name].role==Role.WOLF
 
   def reset(self):
-    self.state = Stage.NIGHT
+    self.state = Stage.NIGHT_BEGIN
     self.rounds = 0
-    self.player = self.players[self.players.keys()[0]]
     for player in self.players:
-      player.set_stat(Stat.ACTIVE)
+      player.set_stat(Stat.ALIVE)
+      player.set_role(Role.VILLAGER)
+
+  def get_alive(self):
+    alive = [name for name,player in self.players.items() if player.stat==Role.ALIVE]
 
   def is_done(self):
     num_alive_villagers = 0
     num_alive_wolves    = 0
-    for player in self.players:
+    for player in self.players.values():
       if player.stat == Stat.DEAD: continue
       if player.role == Role.WOLF: num_alive_wolves += 1
       else:                     num_alive_villagers += 1
